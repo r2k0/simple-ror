c8e4de8167d11e41a21c410909cd7967ba7025a2
r2k0 <r2k0@outlook.com>
2014-09-08 07:17:41 -0700

    devise, add user and install devise views.

--- treebook/db/schema.rb
+++ treebook/db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140907212837) do
+ActiveRecord::Schema.define(:version => 20140908032247) do
 
   create_table "statuses", :force => true do |t|
-    t.string   "name"
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
+
+  add_index "statuses", ["user_id"], :name => "index_statuses_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
