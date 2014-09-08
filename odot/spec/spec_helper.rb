7b0a61dd310f4080b4df3e0cb9e42e2196e5915e
r2k0 <iheartcpp@gmail.com>
2014-09-08 12:33:17 -0700

    adding features directory and tests

--- odot/spec/spec_helper.rb
+++ odot/spec/spec_helper.rb
@@ -20,7 +20,8 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
-  # ## Mock Framework
+	
+	# ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
   #
