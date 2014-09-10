f6cdad1c58c6f794fc885f2b688e0f2328a19b95
r2k0 <iheartcpp@gmail.com>
2014-09-09 19:20:34 -0700

    added edit no title test

--- odot/spec/features/todo_lists/edit_spec.rb
+++ odot/spec/features/todo_lists/edit_spec.rb
@@ -1,21 +1,41 @@
 require 'spec_helper'
 
 describe "Editing todo lists" do
-	it "updates a todo list successfully with correct information" do
-		todo_list = TodoList.create(title: "Groceries", description: "Grocery list.")
+	let!(:todo_list) { TodoList.create(title: "Groceries", description: "Grocery list.")}
+	def update_todo_list(options={})
+		options[:title] ||= "My todo list"
+		options[:description] ||= "This is my todo list."
+
+		todo_list = options[:todo_list]
 		visit "/todo_lists"
 		within "#todo_list_#{todo_list.id}" do
 			click_link "Edit"
 		end
-
-		fill_in "Title", with: "New title"
-		fill_in "Description", with: "New description"
+		
+		fill_in "Title", with: options[:title]
+		fill_in "Description", with: options[:description]
 		click_button "Update Todo list"
 
+	end
+
+	it "updates a todo list successfully with correct information" do
+
+		update_todo_list	todo_list: todo_list, 
+											title: "New title", 
+											description: "New description"
 		todo_list.reload
 
 		expect(page).to have_content("Todo list was successfully updated")
 		expect(todo_list.title).to eq("New title")
 		expect(todo_list.description).to eq("New description")
 	end
+
+	it "displays an error with no title" do
+		update_todo_list todo_list: todo_list, title: ""
+		title = todo_list.title
+		todo_list.reload
+		expect(todo_list.title).to eq(title)
+		expect(page).to have_content("error")
+	end
+
 end
